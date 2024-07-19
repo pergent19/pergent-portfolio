1765d1f77cf3dd488ec2c739475fe4035bff2c6b
unknown <pergent.galang19@gmail.com>
2024-07-19 11:36:28 +0800

    Added about and skill section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Skills from './Skills'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+        <div className='about-container'>
+            <div className='about-header-text'>
+                <h2>About me 🛠️</h2>
+                <p>4+ Years of experience in the <span>IT industry</span>.
+                    I bring expertise in <span>web development, <br />  application modernization, and application support. </span>
+                    I am eager to <br /> leverage my programming skills  to contribute to both  organizational growth <br /> and my personal development.</p>
+                <div className='company-container'>
+                    <p>UniQx AI </p> <span></span>
+                    <p>Accenture</p> <span></span>
+                    <p>Fujitsu</p> <span></span>
+                </div>
+            </div>
+            <div className='about-image-container'>
+                <img src={`${process.env.PUBLIC_URL}/images/dp.jpg`} alt="Skills" className="about-image" />
+            </div>
+        </div>
+        <Skills />
+     </>
   )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -8,7 +8,7 @@ export default function Landing() {
             <p>I'm a <span>full-stack developer</span> specializing in <span>MERN</span> <small className='nanum-pen-script-regular'>(MongoDB, Express.js, React.js, Node.js)</small>   <br /> technologies, driven by a passion for creating compelling and user-friendly experiences. ✨</p>
         </div>
         <div className='landing-image'>
-        <img src={`${process.env.PUBLIC_URL}/landing_coffee.jpg`} alt="Pergent's" className='rounded-image' />
+        <img src={`${process.env.PUBLIC_URL}/images/landing_coffee.jpg`} alt="Pergent's" className='rounded-image' />
         </div>
     </div>
   )
