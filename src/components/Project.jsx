7ca9143be16117473b1d66cdc4514d9690c0bb97
unknown <pergent.galang19@gmail.com>
2024-09-11 17:38:54 +0800

    Added todo app in the project section and fix some css

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,6 +5,7 @@ export default function Project() {
       // Filter projects to include only those with type "fullstack"
     const fullstackProjects = projects.filter(project => project.type === 'fullstack');
     const staticProjects = projects.filter(project => project.type === 'static');
+    const reactProject = projects.filter(project => project.type === 'react');
   return (
     <div className='project-container wrapper' id="projects">
         <div className='project-header-text'>
@@ -26,6 +27,19 @@ export default function Project() {
 
             <h3>Static Website</h3>
             {staticProjects.map((project, index) => (
+                <div key={index}> 
+                    <img className='project-image' src={`${process.env.PUBLIC_URL}/${project.imgSrc}`} alt="bitcapp" />
+                    <div className='overlay'>
+                        <span className='overlay-text'>{project.title}</span>
+                        <p className='overlay-description'>{project.description}</p>
+                        <p className='overlay-skills'>{project.tech}</p>
+                        <a className='btn-project' href={project.link} target="_blank" rel="noopener noreferrer">View Live</a>
+                    </div>
+                </div>
+            ))}
+
+            <h3>ReactJS Application</h3>
+            {reactProject.map((project, index) => (
                 <div key={index}> 
                     <img className='project-image' src={`${process.env.PUBLIC_URL}/${project.imgSrc}`} alt="bitcapp" />
                     <div className='overlay'>
