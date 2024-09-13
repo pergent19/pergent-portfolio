1f9e4e940757ed0d780466afe75aabfcde2ebebd
unknown <pergent.galang19@gmail.com>
2024-09-13 11:12:14 +0800

    Adding tab to the project section, and add animation, and change images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,10 +16,10 @@ export default function Header() {
         <div className={`logo ${isOpen ? 'open-logo' : ''}`}></div>
         <nav className={`nav ${isOpen ? 'open' : ''}`}>
           <ul>
-            <li><a href="#home"><Link to="home" smooth={true} duration={800} onClick={closeMenu}>Home</Link></a></li>
-            <li><a href="#about"><Link to="about" smooth={true} duration={800} onClick={closeMenu}>About</Link></a></li>
-            <li><a href="#projects"><Link to="projects" smooth={true} duration={800} onClick={closeMenu}>Projects</Link></a></li>
-            <li><a href="#contact"><Link to="contact" smooth={true} duration={800} onClick={closeMenu}>Contact</Link></a></li>
+            <li><Link to="home" smooth={true} duration={800} onClick={closeMenu}>Home</Link></li>
+            <li><Link to="about" smooth={true} duration={800} onClick={closeMenu}>About</Link></li>
+            <li><Link to="projects" smooth={true} duration={800} onClick={closeMenu}>Projects</Link></li>
+            <li><Link to="contact" smooth={true} duration={800} onClick={closeMenu}>Contact</Link></li>
           </ul>
         </nav>
         <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,56 +1,58 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import projects from '../objects/projects.json'
 
 export default function Project() {
-      // Filter projects to include only those with type "fullstack"
-    const fullstackProjects = projects.filter(project => project.type === 'fullstack');
-    const staticProjects = projects.filter(project => project.type === 'static');
-    const reactProject = projects.filter(project => project.type === 'react');
+    const [activeTab, setActiveTab] = useState('All');
+    const [contentVisible, setContentVisible] = useState(false);
+
+    const tabs = ['All', 'React/Node', 'React', 'Static'];
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab);
+    };
+
+    const filteredProject = activeTab === 'All' ? projects : projects.filter(project => project.type === activeTab);
+
+    useEffect(() => {
+        
+        setContentVisible(false); 
+        const timer = setTimeout(() => setContentVisible(true), 400); 
+        return () => clearTimeout(timer); 
+    }, [activeTab]);
+
   return (
     <div className='project-container wrapper' id="projects">
         <div className='project-header-text'>
             <h2>My Works 👨‍💻</h2>
         </div>
-        <div className='project-image-container'>
-            <h3>Fullstack Application</h3>
-            {fullstackProjects.map((project, index) => (
-                <div key={index}> 
-                    <img className='project-image' src={`${process.env.PUBLIC_URL}/${project.imgSrc}`} alt="bitcapp" />
-                    <div className='overlay'>
-                        <span className='overlay-text'>{project.title}</span>
-                        <p className='overlay-description'>{project.description}</p>
-                        <p className='overlay-skills'>{project.tech}</p>
-                        <a className='btn-project' href={project.link} target="_blank" rel="noopener noreferrer">View Live</a>
-                    </div>
-                </div>
-            ))}
 
-            <h3>Static Website</h3>
-            {staticProjects.map((project, index) => (
-                <div key={index}> 
-                    <img className='project-image' src={`${process.env.PUBLIC_URL}/${project.imgSrc}`} alt="bitcapp" />
-                    <div className='overlay'>
-                        <span className='overlay-text'>{project.title}</span>
-                        <p className='overlay-description'>{project.description}</p>
-                        <p className='overlay-skills'>{project.tech}</p>
-                        <a className='btn-project' href={project.link} target="_blank" rel="noopener noreferrer">View Live</a>
-                    </div>
-                </div>
-            ))}
-
-            <h3>ReactJS Application</h3>
-            {reactProject.map((project, index) => (
-                <div key={index}> 
-                    <img className='project-image' src={`${process.env.PUBLIC_URL}/${project.imgSrc}`} alt="bitcapp" />
-                    <div className='overlay'>
-                        <span className='overlay-text'>{project.title}</span>
-                        <p className='overlay-description'>{project.description}</p>
-                        <p className='overlay-skills'>{project.tech}</p>
-                        <a className='btn-project' href={project.link} target="_blank" rel="noopener noreferrer">View Live</a>
-                    </div>
-                </div>
-            ))}
+        <div className="tab-container">
+            <div className="tab-list">
+                {tabs.map((tab) => (
+                <button
+                    key={tab}
+                    className={`tab ${activeTab === tab ? 'active' : ''}`}
+                    onClick={() => handleTabClick(tab)}
+                >
+                    {tab}
+                </button>
+                ))}
+            </div>
         </div>
+
+        <div className={`project-image-container ${contentVisible ? 'visible' : ''}`}>
+                {filteredProject.map((project, index) => (
+                    <div key={index}> 
+                        <img className='project-image' src={`${process.env.PUBLIC_URL}/${project.imgSrc}`} alt="project" />
+                        <div className='overlay'>
+                            <span className='overlay-text'>{project.title}</span>
+                            <p className='overlay-description'>{project.description}</p>
+                            <p className='overlay-skills'>{project.tech}</p>
+                            <a className='btn-project' href={project.link} target="_blank" rel="noopener noreferrer">View Live</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
     </div>
   )
 }
