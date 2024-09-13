d93907703903f6b097946bad6ed66649f42d4d03
unknown <pergent.galang19@gmail.com>
2024-09-13 17:00:31 +0800

    Add time line component and add some skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Skills from './Skills'
 import './About.css'
+import Timeline from './Timelime'
 
 export default function About() {
   return (
@@ -11,15 +12,16 @@ export default function About() {
                 <p>4+ Years of experience in the <span>IT industry</span>.
                     I bring expertise in <span>web development, <br />  application modernization, and application support. </span>
                     I am eager to <br /> leverage my programming skills  to contribute to both  organizational growth <br /> and my personal development.</p>
-                <div className='company-container'>
+                {/* <div className='company-container'>
                     <p>UniQx AI </p> <span></span>
                     <p>Accenture</p> <span></span>
                     <p>Fujitsu</p> <span></span>
-                </div>
+                </div> */}
             </div>
             <div className='about-image-container'>
-                <img src={`${process.env.PUBLIC_URL}/images/dp.jpg`} alt="Skills" className="about-image" />
+                <img src={`${process.env.PUBLIC_URL}/images/dp_2.jpg`} alt="Skills" className="about-image" />
             </div>
+            <Timeline />
         </div>
         <Skills />
      </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Skills.css'
+import skills from '../objects/skills.json'
 
 export default function Skills() {
   return (
@@ -8,18 +9,9 @@ export default function Skills() {
             <h2>Tech 🖥️</h2>
         </div>
         <ul className='skills nanum-pen-script-regular'>
-            <li><img src={`${process.env.PUBLIC_URL}/images/reactjs.png`} alt="" />ReactJS</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/html5.png`} alt="" />HTML</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/css.png`} alt="" />CSS</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/js.png`} alt="" />Javascript </li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/boots.png`} alt="" />Bootstrap</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/java.png`} alt="" />Java</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/spring.png`} alt="" />Spring</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/sass.png`} alt="" />Sass</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/mongodb.png`} alt="" />MognoDB </li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/nodejs.png`} alt="" />Nodejs</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/laravel.png`} alt="" />Laravel</li>
-            <li><img src={`${process.env.PUBLIC_URL}/images/api.png`} alt="" />API </li>
+          {skills.map((skill => (
+            <li><img src={`${process.env.PUBLIC_URL}/images/skills/${skill.img}`} alt="" />{skill.name}</li>
+          )))}
         </ul>
     </div>
   )
